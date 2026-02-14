e3187ccc41e4e672bb3ce107e72fb36f25cc48a7
Amanuel <amanuelfrc@gmail.com>
2026-02-14 11:28:04 -0800

    fiexed

--- app/components/works.tsx
+++ app/components/works.tsx
@@ -3,7 +3,8 @@ import Image from "next/image";
 import Link from "next/link";
 import { useState } from "react";
 import WorkModal from "./workmodal";
-import { works } from "@/data/works";
+import { works } from "../data/works";
+
 export default function Works() {
   const [shouldShowWorkModal, setShouldShowWorkModal] =
     useState<boolean>(false);
