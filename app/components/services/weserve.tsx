3c7349076ddff07d4e847c977b801e9dd7b2b119
unknown <amanuelfrc@gmail.com>
2026-03-09 17:00:05 +0300

    'changed-ui'

--- app/components/services/weserve.tsx
+++ app/components/services/weserve.tsx
@@ -5,14 +5,14 @@ export default function WeServe() {
                 <div className=" lg:max-w-7xl xl:max-w-350 mx-auto  " >
 
                     <div className="grid md:grid-cols-2 gap-10 justify-center 0">
-                        <div className="h-full md:flex md:items-center md:justify-center">
-                            <div className="text-white">
+                        <div className="md:h-1/2  md:sticky md:top-20 md:flex md:items-center md:justify-center md:p-20" style={{}}>
+                            <div className="text-white stiky top-20">
                                 <p className="md:border-l-8 md:my-7 md:border-l-blue-600 md:pl-5 text-2xl font-semibold text-center my-1 inline-block">
-                                    <p className="w-full p-3 text-justify text-xl">We stay updated with the latest trends and technologies to ensrue your website is modern, secure, and optimized for the best resutls.        </p>
+                                    <p className="w-full p-3  md:text-3xl">We stay updated with the latest trends and technologies to ensrue your website is modern, secure, and optimized for the best resutls.        </p>
                                 </p>
                             </div>
                         </div>
-                        <div className="grid md:grid-cols-3 grid-cols-2 gap-6 ">
+                        <div className="grid md:grid-cols-2 grid-cols-2 gap-6 ">
                             <div className=" h-40 md:rounded-lg rounded-md bg-white p-6 shadow-xl text-center flex items-center justify-center">
                                 <div className="flex flex-col space-y-2">
                                     <p className="font-semibold text-lg">Education.</p>
